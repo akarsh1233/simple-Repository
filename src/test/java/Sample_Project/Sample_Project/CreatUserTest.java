f74087284797d56c886a4c73858ddf3851424004
akarsh1233 <akarsh.s.m.47@gmail.com>
2022-05-10 13:40:19 +0530

    aka

---
+++ src/test/java/Sample_Project/Sample_Project/CreatUserTest.java
@@ -0,0 +1,16 @@
+package Sample_Project.Sample_Project;
+
+import org.testng.annotations.Test;
+
+public class CreatUserTest {
+	
+	@Test
+	
+	public void creatusertest() {
+		System.out.println("creatuser");
+		
+	}
+	
+}
+
+
---
+++ src/test/java/Sample_Project/Sample_Project/ReadingDatafromCMD.java
@@ -0,0 +1,29 @@
+package Sample_Project.Sample_Project;
+
+import org.apache.commons.collections4.bag.SynchronizedSortedBag;
+import org.testng.annotations.Test;
+
+public class ReadingDatafromCMD {
+	@Test(groups = "smoke")
+	
+	public void readingdatafromcmd() {
+	String URL = System.getProperty("url");
+	String un = System.getProperty("username");
+	String pw =System.getProperty("password");
+	System.out.println(URL);
+	System.out.println(un);
+	System.out.println(pw);
+	
+	
+	}
+	@Test(groups = "regression")
+	
+	public void xy() {
+	String kak = System.getProperty("kak");
+	System.out.println(kak);
+		
+		
+	
+
+}
+}
